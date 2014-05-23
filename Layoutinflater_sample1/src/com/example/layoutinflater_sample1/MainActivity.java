5f1aa2d6ffde720d4f93be95c2468fa4f2b9561a
nssv-05 <nssv-05@SO-PC-116>
2014-05-23 13:32:56 +0900

    コメント追加
    コミットテストのため，コメントを追加した．

---
+++ Layoutinflater_sample1/src/com/example/layoutinflater_sample1/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.layoutinflater_sample1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
+
+
+	//コメントテスト
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+               super.onCreate(savedInstanceState);
+               setContentView(R.layout.activity_main);
+
+               final LinearLayout layout = (LinearLayout)findViewById(R.id.linearLayout1);
+
+               findViewById(R.id.button1).setOnClickListener(new OnClickListener() {
+
+                     @Override
+                     public void onClick(View v) {
+                           layout.removeAllViews();
+                           getLayoutInflater().inflate(R.layout.layout1, layout);
+                     }
+               });
+
+               findViewById(R.id.button2).setOnClickListener(new OnClickListener() {
+
+                      @Override
+                      public void onClick(View v) {
+                            layout.removeAllViews();
+                            getLayoutInflater().inflate(R.layout.layout2, layout);
+                     }
+                });
+           }
+ }
